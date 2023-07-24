75b4e45903601725683302a990bbcdb9e7104083
echooocc <echo2go@gmail.com>
2023-07-24 18:46:16 -0400

    add upload api & test cases

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,11 @@
 import express from 'express';
 import multer from 'multer';
-
 const app = express();
 const upload = multer({ dest: 'uploads/' });
 
 // endpoint upload dicom file
 app.post('/api/upload', upload.single('file'), (req, res) => {
-
-  console.log('this ran');
-  res.status(200).json({ fileId: req?.file?.filename });
+  res.status(201).json({ fileId: req?.file?.filename });
 });
 
-
-app.listen(3000, () => console.log('Server listening on port 3000'));
+export default app
--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -1,4 +1,17 @@
+import request from 'supertest';
+import app from "../src/app";
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(1).toBe(1);
+describe('dicom api tests', () => {
+  test("/api/upload", (done) => {
+    request(app).post("/api/upload")
+    .attach('file', `${__dirname}/mock/IM000002`)
+    .expect(201)
+    .expect((res) => {
+      res.body.fileId.length !== 0
+    })
+    .end((err) => {
+      if (err) return done(err);
+      return done();
+    });
+});
 });
