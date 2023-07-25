f5a9432761529fbfc68507e908407f2ce3382563
echooocc <echo2go@gmail.com>
2023-07-24 22:25:59 -0400

    add get api/dicom by tag

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,36 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import multer from 'multer';
+import path from 'path';
+import fs from "fs";
+import dicomParser from "dicom-parser";
+
 const app = express();
 const upload = multer({ dest: 'uploads/' });
 
-// endpoint upload dicom file
-app.post('/api/upload', upload.single('file'), (req, res) => {
+app.post('/api/upload', upload.single('file'), (req: Request, res: Response) => {
   res.status(201).json({ fileId: req?.file?.filename });
+});
+
+
+// dicomTag is a string 8 digits, https://www.dicomlibrary.com/dicom/dicom-tags/
+// 00100010 Patient's Name
+// 00100020	Patient ID
+// 00200013	Instance Number
+app.get('/api/dicom', (req: Request, res: Response) => {
+  const fileId= req.query.fileId as string;
+  const dicomTag = req.query.dicomTag as string;
+  const filePath = path.join(__dirname, '../uploads', fileId);
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.status(500).send('error reading file');
+      return; 
+    }
+
+    const dataSet = dicomParser.parseDicom(data);
+    const attribute = dataSet.string('x'+dicomTag);
+
+    res.status(200).json({ dicomTag: dicomTag, value: attribute });
+  });
 });
 
 export default app
--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -1,8 +1,9 @@
 import request from 'supertest';
 import app from "../src/app";
 
+
 describe('dicom api tests', () => {
-  test("/api/upload", (done) => {
+  xtest("/api/upload", (done) => {
     request(app).post("/api/upload")
     .attach('file', `${__dirname}/mock/IM000002`)
     .expect(201)
@@ -13,5 +14,22 @@ describe('dicom api tests', () => {
       if (err) return done(err);
       return done();
     });
-});
+  });
+
+  const fileId = 'd751b79ae581e6c36569589db2358b8b';
+  const dicomTagPatientName = '00100010';
+  test(`/api/dicom by dicomTag=${dicomTagPatientName}`, async () => {
+    const response = await request(app).get("/api/dicom/?fileId=" + fileId + "&dicomTag=" + dicomTagPatientName)
+    .expect(200);
+    expect(response.body.dicomTag).toBe(dicomTagPatientName);
+    expect(response.body.value).toBe('NAYYAR^HARSH')
+  });
+
+  const dicomTagPatientId = '00100020';
+  test(`/api/dicom by dicomTag=${dicomTagPatientId}`, async () => {
+    const response = await request(app).get("/api/dicom/?fileId=" + fileId + "&dicomTag=" + dicomTagPatientId)
+    .expect(200);
+    expect(response.body.dicomTag).toBe(dicomTagPatientId);
+    expect(response.body.value).toBe('5184')
+  });
 });
